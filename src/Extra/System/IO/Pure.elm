wise-tim/elm-compiler-in-elm-ui

Add folds and traversals using loop

src/Extra/System/IO/Pure.elm
@@ -5,15 +5,26 @@ module Extra.System.IO.Pure exposing
     , andThen
     , bind
     , fmap
+    , foldlMList
+    , foldlMMap
+    , foldrMList
+    , forMList_
+    , imapMList_
     , liftA2
     , liftM
     , liftM2
     , liftM3
     , liftS
     , loop
+    , mapMList_
+    , mapMMap_
     , performIO
     , pure
     , return
+    , sequenceAList
+    , traverseList
+    , traverseMap
+    , traverseWithKey
     , when
     )
 
@@ -22,6 +33,8 @@ module Extra.System.IO.Pure exposing
 import Extra.Class.Applicative as Applicative
 import Extra.Class.Functor as Functor
 import Extra.Class.Monad as Monad
+import Extra.Type.List as MList exposing (TList)
+import Extra.Type.Map as Map
 
 
 type alias IO s a =
@@ -129,3 +142,162 @@ loop callback loopState ioState =
 
         ( Done a, newIOState ) ->
             ( a, newIOState )
+
+
+
+-- FOLD AND TRAVERSAL
+
+
+foldlMList : (b -> a -> IO s b) -> b -> TList a -> IO s b
+foldlMList callback zero list =
+    loop (foldlMHelpList callback) ( MList.reverse list, zero )
+
+
+foldlMHelpList : (b -> a -> IO s b) -> ( TList a, b ) -> IO s (Step ( TList a, b ) b)
+foldlMHelpList callback ( list, result ) =
+    case list of
+        [] ->
+            return (Done result)
+
+        a :: rest ->
+            fmap (\b -> Loop ( rest, b )) (callback result a)
+
+
+foldrMList : (a -> b -> IO s b) -> b -> TList a -> IO s b
+foldrMList callback zero list =
+    loop (foldrMHelpList callback) ( list, zero )
+
+
+foldrMHelpList : (a -> b -> IO s b) -> ( TList a, b ) -> IO s (Step ( TList a, b ) b)
+foldrMHelpList callback ( list, result ) =
+    case list of
+        [] ->
+            return (Done result)
+
+        a :: rest ->
+            fmap (\b -> Loop ( rest, b )) (callback a result)
+
+
+forMList_ : TList a -> (a -> IO s b) -> IO s ()
+forMList_ list callback =
+    loop (mapMListHelp_ callback) ( MList.reverse list, pure () )
+
+
+imapMList_ : (Int -> a -> IO s b) -> TList a -> IO s ()
+imapMList_ callback list =
+    loop (imapMListHelp_ callback) ( MList.reverse list, MList.length list - 1, pure () )
+
+
+imapMListHelp_ : (Int -> a -> IO s b) -> ( TList a, Int, IO s () ) -> IO s (Step ( TList a, Int, IO s () ) ())
+imapMListHelp_ callback ( list, index, result ) =
+    case list of
+        [] ->
+            fmap Done result
+
+        a :: rest ->
+            return (Loop ( rest, index - 1, bind (callback index a) (\_ -> result) ))
+
+
+mapMList_ : (a -> IO s b) -> TList a -> IO s ()
+mapMList_ callback list =
+    loop (mapMListHelp_ callback) ( MList.reverse list, pure () )
+
+
+mapMListHelp_ : (a -> IO s b) -> ( TList a, IO s () ) -> IO s (Step ( TList a, IO s () ) ())
+mapMListHelp_ callback ( list, result ) =
+    case list of
+        [] ->
+            fmap Done result
+
+        a :: rest ->
+            fmap (\_ -> Loop ( rest, result )) (callback a)
+
+
+sequenceAList : TList (IO s a) -> IO s (TList a)
+sequenceAList list =
+    loop sequenceAListHelp ( list, [] )
+
+
+sequenceAListHelp : ( TList (IO s a), TList a ) -> IO s (Step ( TList (IO s a), TList a ) (TList a))
+sequenceAListHelp ( list, result ) =
+    case list of
+        [] ->
+            return (Done (List.reverse result))
+
+        io :: rest ->
+            fmap (\a -> Loop ( rest, a :: result )) io
+
+
+traverseList : (a -> IO s b) -> TList a -> IO s (TList b)
+traverseList callback list =
+    loop (traverseListHelp callback) ( list, [] )
+
+
+traverseListHelp : (a -> IO s b) -> ( TList a, TList b ) -> IO s (Step ( TList a, TList b ) (TList b))
+traverseListHelp callback ( list, result ) =
+    case list of
+        [] ->
+            return (Done (List.reverse result))
+
+        a :: rest ->
+            fmap (\b -> Loop ( rest, b :: result )) (callback a)
+
+
+foldlMMap : (b -> a -> IO s b) -> b -> Map.Map comparable a -> IO s b
+foldlMMap callback zero map =
+    loop (foldlMHelpMap callback) ( MList.reverse (Map.toList map), zero )
+
+
+foldlMHelpMap : (b -> a -> IO s b) -> ( TList ( comparable, a ), b ) -> IO s (Step ( TList ( comparable, a ), b ) b)
+foldlMHelpMap callback ( pairs, result ) =
+    case pairs of
+        [] ->
+            return (Done result)
+
+        ( _, a ) :: rest ->
+            fmap (\b -> Loop ( rest, b )) (callback result a)
+
+
+mapMMap_ : (a -> IO s b) -> Map.Map comparable a -> IO s ()
+mapMMap_ callback map =
+    loop (mapMMapHelp_ callback) ( MList.reverse (Map.toList map), pure () )
+
+
+mapMMapHelp_ : (a -> IO s b) -> ( TList ( comparable, a ), IO s () ) -> IO s (Step ( TList ( comparable, a ), IO s () ) ())
+mapMMapHelp_ callback ( list, result ) =
+    case list of
+        [] ->
+            fmap Done result
+
+        ( _, a ) :: rest ->
+            fmap (\_ -> Loop ( rest, result )) (callback a)
+
+
+traverseMap : (a -> IO s b) -> Map.Map comparable a -> IO s (Map.Map comparable b)
+traverseMap callback map =
+    loop (traverseMapHelp callback) ( Map.toList map, Map.empty )
+
+
+traverseMapHelp : (a -> IO s b) -> ( TList ( comparable, a ), Map.Map comparable b ) -> IO s (Step ( TList ( comparable, a ), Map.Map comparable b ) (Map.Map comparable b))
+traverseMapHelp callback ( pairs, result ) =
+    case pairs of
+        [] ->
+            return (Done result)
+
+        ( k, a ) :: rest ->
+            fmap (\b -> Loop ( rest, Map.insert k b result )) (callback a)
+
+
+traverseWithKey : (comparable -> a -> IO s b) -> Map.Map comparable a -> IO s (Map.Map comparable b)
+traverseWithKey callback map =
+    loop (traverseWithKeyHelp callback) ( Map.toList map, Map.empty )
+
+
+traverseWithKeyHelp : (comparable -> a -> IO s b) -> ( TList ( comparable, a ), Map.Map comparable b ) -> IO s (Step ( TList ( comparable, a ), Map.Map comparable b ) (Map.Map comparable b))
+traverseWithKeyHelp callback ( pairs, result ) =
+    case pairs of
+        [] ->
+            return (Done result)
+
+        ( k, a ) :: rest ->
+            fmap (\b -> Loop ( rest, Map.insert k b result )) (callback k a)

pithub/elm-compiler-in-elm

src/Extra/System/IO/Pure.elm
@@ -5,15 +5,26 @@ module Extra.System.IO.Pure exposing
     , andThen
     , bind
     , fmap
+    , foldlMList
+    , foldlMMap
+    , foldrMList
+    , forMList_
+    , imapMList_
     , liftA2
     , liftM
     , liftM2
     , liftM3
     , liftS
     , loop
+    , mapMList_
+    , mapMMap_
     , performIO
     , pure
     , return
+    , sequenceAList
+    , traverseList
+    , traverseMap
+    , traverseWithKey
     , when
     )
 
@@ -22,6 +33,8 @@ module Extra.System.IO.Pure exposing
 import Extra.Class.Applicative as Applicative
 import Extra.Class.Functor as Functor
 import Extra.Class.Monad as Monad
+import Extra.Type.List as MList exposing (TList)
+import Extra.Type.Map as Map
 
 
 type alias IO s a =
@@ -129,3 +142,162 @@ loop callback loopState ioState =
 
         ( Done a, newIOState ) ->
             ( a, newIOState )
+
+
+
+-- FOLD AND TRAVERSAL
+
+
+foldlMList : (b -> a -> IO s b) -> b -> TList a -> IO s b
+foldlMList callback zero list =
+    loop (foldlMHelpList callback) ( list, zero )
+
+
+foldlMHelpList : (b -> a -> IO s b) -> ( TList a, b ) -> IO s (Step ( TList a, b ) b)
+foldlMHelpList callback ( list, result ) =
+    case list of
+        [] ->
+            return (Done result)
+
+        a :: rest ->
+            fmap (\b -> Loop ( rest, b )) (callback result a)
+
+
+foldrMList : (a -> b -> IO s b) -> b -> TList a -> IO s b
+foldrMList callback zero list =
+    loop (foldrMHelpList callback) ( MList.reverse list, zero )
+
+
+foldrMHelpList : (a -> b -> IO s b) -> ( TList a, b ) -> IO s (Step ( TList a, b ) b)
+foldrMHelpList callback ( list, result ) =
+    case list of
+        [] ->
+            return (Done result)
+
+        a :: rest ->
+            fmap (\b -> Loop ( rest, b )) (callback a result)
+
+
+forMList_ : TList a -> (a -> IO s b) -> IO s ()
+forMList_ list callback =
+    loop (mapMListHelp_ callback) ( list, pure () )
+
+
+imapMList_ : (Int -> a -> IO s b) -> TList a -> IO s ()
+imapMList_ callback list =
+    loop (imapMListHelp_ callback) ( MList.reverse list, MList.length list - 1, pure () )
+
+
+imapMListHelp_ : (Int -> a -> IO s b) -> ( TList a, Int, IO s () ) -> IO s (Step ( TList a, Int, IO s () ) ())
+imapMListHelp_ callback ( list, index, result ) =
+    case list of
+        [] ->
+            fmap Done result
+
+        a :: rest ->
+            return (Loop ( rest, index - 1, bind (callback index a) (\_ -> result) ))
+
+
+mapMList_ : (a -> IO s b) -> TList a -> IO s ()
+mapMList_ callback list =
+    loop (mapMListHelp_ callback) ( list, pure () )
+
+
+mapMListHelp_ : (a -> IO s b) -> ( TList a, IO s () ) -> IO s (Step ( TList a, IO s () ) ())
+mapMListHelp_ callback ( list, result ) =
+    case list of
+        [] ->
+            fmap Done result
+
+        a :: rest ->
+            fmap (\_ -> Loop ( rest, result )) (callback a)
+
+
+sequenceAList : TList (IO s a) -> IO s (TList a)
+sequenceAList list =
+    loop sequenceAListHelp ( list, [] )
+
+
+sequenceAListHelp : ( TList (IO s a), TList a ) -> IO s (Step ( TList (IO s a), TList a ) (TList a))
+sequenceAListHelp ( list, result ) =
+    case list of
+        [] ->
+            return (Done (List.reverse result))
+
+        io :: rest ->
+            fmap (\a -> Loop ( rest, a :: result )) io
+
+
+traverseList : (a -> IO s b) -> TList a -> IO s (TList b)
+traverseList callback list =
+    loop (traverseListHelp callback) ( list, [] )
+
+
+traverseListHelp : (a -> IO s b) -> ( TList a, TList b ) -> IO s (Step ( TList a, TList b ) (TList b))
+traverseListHelp callback ( list, result ) =
+    case list of
+        [] ->
+            return (Done (List.reverse result))
+
+        a :: rest ->
+            fmap (\b -> Loop ( rest, b :: result )) (callback a)
+
+
+foldlMMap : (b -> a -> IO s b) -> b -> Map.Map comparable a -> IO s b
+foldlMMap callback zero map =
+    loop (foldlMHelpMap callback) ( Map.toList map, zero )
+
+
+foldlMHelpMap : (b -> a -> IO s b) -> ( TList ( comparable, a ), b ) -> IO s (Step ( TList ( comparable, a ), b ) b)
+foldlMHelpMap callback ( pairs, result ) =
+    case pairs of
+        [] ->
+            return (Done result)
+
+        ( _, a ) :: rest ->
+            fmap (\b -> Loop ( rest, b )) (callback result a)
+
+
+mapMMap_ : (a -> IO s b) -> Map.Map comparable a -> IO s ()
+mapMMap_ callback map =
+    loop (mapMMapHelp_ callback) ( Map.toList map, pure () )
+
+
+mapMMapHelp_ : (a -> IO s b) -> ( TList ( comparable, a ), IO s () ) -> IO s (Step ( TList ( comparable, a ), IO s () ) ())
+mapMMapHelp_ callback ( list, result ) =
+    case list of
+        [] ->
+            fmap Done result
+
+        ( _, a ) :: rest ->
+            fmap (\_ -> Loop ( rest, result )) (callback a)
+
+
+traverseMap : (a -> IO s b) -> Map.Map comparable a -> IO s (Map.Map comparable b)
+traverseMap callback map =
+    loop (traverseMapHelp callback) ( Map.toList map, Map.empty )
+
+
+traverseMapHelp : (a -> IO s b) -> ( TList ( comparable, a ), Map.Map comparable b ) -> IO s (Step ( TList ( comparable, a ), Map.Map comparable b ) (Map.Map comparable b))
+traverseMapHelp callback ( pairs, result ) =
+    case pairs of
+        [] ->
+            return (Done result)
+
+        ( k, a ) :: rest ->
+            fmap (\b -> Loop ( rest, Map.insert k b result )) (callback a)
+
+
+traverseWithKey : (comparable -> a -> IO s b) -> Map.Map comparable a -> IO s (Map.Map comparable b)
+traverseWithKey callback map =
+    loop (traverseWithKeyHelp callback) ( Map.toList map, Map.empty )
+
+
+traverseWithKeyHelp : (comparable -> a -> IO s b) -> ( TList ( comparable, a ), Map.Map comparable b ) -> IO s (Step ( TList ( comparable, a ), Map.Map comparable b ) (Map.Map comparable b))
+traverseWithKeyHelp callback ( pairs, result ) =
+    case pairs of
+        [] ->
+            return (Done result)
+
+        ( k, a ) :: rest ->
+            fmap (\b -> Loop ( rest, Map.insert k b result )) (callback k a)